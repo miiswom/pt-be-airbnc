e414b0712791602578ff77a32aa88af8cd82f827
Farmata Kane <kane.farmata@gmail.com>
2024-12-23 21:17:33 +0000

    Add a static html webpage

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path")
 const { handleNotFound, handleBadRequest, handleInvalidEndpoint } = require("./controllers/errors/handlingErrors")
 const app = express();
 const apiRouter  = require("./routers/api.router");
 
 app.use(express.json());
+app.use("/index", express.static(path.join(__dirname, 'public')))
+
 app.use("/api", apiRouter);
 
 // invalid enpoint //
