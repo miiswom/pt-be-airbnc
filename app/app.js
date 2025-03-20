630554d205b177d911730717e8c6571a6409f381
Farmata Kane <kane.farmata@gmail.com>
2025-03-20 09:44:59 +0000

    Configure cors origin to "localhost:5173"

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use("/index", express.static(path.join(__dirname, 'public')))
 app.use(cors({
-  origin: `http://localhost:10000`,
+  origin: `http://localhost:5173`,
   credentials: true
 }))
 app.use("/api", apiRouter);
