1764e228052b619f1e56332f370ba0c70b0e8807
Farmata Kane <kane.farmata@gmail.com>
2025-03-20 08:20:46 +0000

    console.log a secret

--- app/controllers/utils/generateTokens.js
+++ app/controllers/utils/generateTokens.js
@@ -2,6 +2,7 @@
 const { TOKEN_SECRET } = process.env;
 const jwt = require("jsonwebtoken")
 
+console.log( TOKEN_SECRET )
 exports.generateAcessToken = (user) => {
   return jwt.sign({ user }, TOKEN_SECRET, { expiresIn: "20s" })
 };
