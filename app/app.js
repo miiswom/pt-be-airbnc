4a8578a50c14cc06fcfd882044e3352974c8b02c
Farmata Kane <kane.farmata@gmail.com>
2025-03-21 05:34:33 +0000

    *

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ app.use("/index", express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 
 app.use(cors({
-  origin: [`http://localhost:5173`],
+  origin: `http://localhost:5173`,
   credentials: "include"
 }));
 
