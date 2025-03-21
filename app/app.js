a210885c5998960f182734cfa8d91dae51092e89
Farmata Kane <kane.farmata@gmail.com>
2025-03-21 06:09:37 +0000

    Remove middleware that set header

--- app/app.js
+++ app/app.js
@@ -19,12 +19,12 @@ app.use(cors({
   credentials: "include"
 }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   next()
+// })
 
 app.use("/api", apiRouter);
 
