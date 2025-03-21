e5fc167c464feea823d273607f9ba3148a3b1fec
Farmata Kane <kane.farmata@gmail.com>
2025-03-21 04:49:26 +0000

    Add middleware to set headers

--- app/app.js
+++ app/app.js
@@ -16,11 +16,18 @@ app.use(cookieParser())
 
 app.use(cors({
   origin: [`http://localhost:5173`],
-  credentials: "include",
-  header : {
-    "Access-Control-Allow-Credentials":true
-  }
-}))
+  credentials: "include"
+}));
+
+app.use((req, res, next) => {
+  res.header('Content-Type', 'application/json;charset=UTF-8')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 app.use("/api", apiRouter);
 
